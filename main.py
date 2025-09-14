432bd6bba267909dfbffb21326bcf49d72ea5753
Ivanchik05 <bozvanovivan@gmail.com>
2025-09-14 16:51:45 +0300

    add first main.py file

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+        self.setFixedSize(QSize(400, 300))
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
